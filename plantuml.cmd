21ec239b0ff97cb33e849381d8c9cbcd5068d1c9
surumjur <juris.kristaps.surums@tieto.com>
2026-07-20 20:18:51 +0300

    great success

--- plantuml.cmd
+++
@@ -1,7 +0,0 @@
-set _BUNDLE=%~dp0
-set _TEMPLATES=%_BUNDLE%cs.ext.md_bundle
-set _JAVA=java
-set GRAPHVIZ_DOT="%_BUNDLE%bin\dot.exe"
-set _PLANTUMLJAR=%_BUNDLE%plantuml.8055.jar
-
-"%_JAVA%" -Djava.awt.headless=true -jar "%_PLANTUMLJAR%" -config "%_TEMPLATES%\tieto_templates\plantuml.cfg" -v -o "%~dp1." "%1"
